cf35ad076c839714c4f88084333d9b4e57d61d1b
ms787 <jajalope372@gmail.com>
2017-10-26 08:50:21 -0400

    v0.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,19 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        
+        int mode = 1;  //decides mode, I guess
+        Scanner mo = new Scanner(System.in);
+        while (mode!=0) {
+            System.out.println("What would you like to do next?");
+            System.out.println("0 to exit, 1 to add a new barcode, 2 to check price, 3 to set budget");
+            mode = mo.nextInt();
+            if ()
 
+        }
     }
 }
