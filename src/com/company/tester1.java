a48c822471372e1a2817886cc63382a0a05a395f
Luke <Lyspeers@gmail.com>
2017-10-27 22:50:57 -0400

    Entire UI number pad example

--- src/com/company/tester1.java
+++ src/com/company/tester1.java
@@ -4,12 +4,15 @@ import java.awt.event.*;
 
 public class tester1 extends JFrame implements ActionListener {
     //awt is the basic ui interface for java
-    //swing is more cusomizable
+    //swing is more custom friendly
 
     JTextField tf;
     JButton b1, b2, b3, b4, b5, b6, b7, b8, b9, b0;
     String op = "";
     public tester1(){
+
+        //Creation of button and text inout fields.  Text Field is uneditable but that can be changed easily
+        //use this basic structure for stuff being pressed that makes stuff happen
 
         tf = new JTextField();
         tf.setBounds(100, 500, 300, 20);
@@ -18,30 +21,39 @@ public class tester1 extends JFrame implements ActionListener {
         b1 = new JButton("1");
         b1.setBounds(50, 50, 125, 75);
         b1.addActionListener(this);
+
         b2 = new JButton("2");
         b2.setBounds(175, 50, 125, 75);
         b2.addActionListener(this);
+
         b3 = new JButton("3");
         b3.setBounds(300, 50, 125, 75);
         b3.addActionListener(this);
+
         b4 = new JButton("4");
         b4.setBounds(50, 150, 125, 75);
         b4.addActionListener(this);
+
         b5 = new JButton("5");
         b5.setBounds(175, 150, 125, 75);
         b5.addActionListener(this);
+
         b6 = new JButton("6");
         b6.setBounds(300, 150, 125, 75);
         b6.addActionListener(this);
+
         b7 = new JButton("7");
         b7.setBounds(50, 250, 125, 75);
         b7.addActionListener(this);
+
         b8 = new JButton("8");
         b8.setBounds(175, 250, 125, 75);
         b8.addActionListener(this);
+
         b9 = new JButton("9");
         b9.setBounds(300, 250, 125, 75);
         b9.addActionListener(this);
+
         b0 = new JButton("0");
         b0.setBounds(175, 350, 125, 75);
         b0.addActionListener(this);
--- src/com/company/tester2.java
+++ src/com/company/tester2.java
@@ -1,4 +1,5 @@
 package com.company;
 
 public class tester2 {
+    //another test space for ui or tech stuffs
 }
